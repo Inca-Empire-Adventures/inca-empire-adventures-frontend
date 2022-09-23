139b585b1d6fb565210c36e15e30cccbe6a76a05
Setsuke <sergio_alva99@hotmail.com>
2022-09-23 01:29:38 -0500

    HU-01 agregar details

--- src/app/pages/character-selection/character-selection.component.ts
+++ src/app/pages/character-selection/character-selection.component.ts
@@ -11,10 +11,20 @@ export class CharacterSelectionComponent implements OnInit {
   title = 'INCA EMPIRE ADVENTURES';
   characters: Character[] = [];
   images: string[];
+  race!: string;
+  profession!: string;
+  equipment!: string;
+  strength!: string;
+  intelligence!: string;
+  dexterty!: string;
+  charisma!: string;
+  wisdom!: string;
+  constitution!: string;
+  hp!: string;
 
   constructor(
     private characterSelectionService: CharacterSelectionService
-  ) { 
+  ) {
     this.images = [
       'null',
       'assets/DiosSol.png',
@@ -37,6 +47,69 @@ export class CharacterSelectionComponent implements OnInit {
         console.log("DATA: ", data)
       }
     )
+  }
+  getCharacterN(id: number) {
+    if (id == 1) {
+      this.race = "Dwarf";
+      this.profession = "Wizard";
+      this.equipment = "Fire Cloak";
+      this.strength = "3";
+      this.intelligence = "5";
+      this.dexterty = "2";
+      this.charisma = "2";
+      this.wisdom = "5";
+      this.constitution = "5";
+      this.hp = "120";
+    }
+    if (id == 2) {
+      this.race = "Dragonborn";
+      this.profession = "Rogue";
+      this.equipment = "Death Mask";
+      this.strength = "5";
+      this.intelligence = "5";
+      this.dexterty = "4";
+      this.charisma = "2";
+      this.wisdom = "5";
+      this.constitution = "5";
+      this.hp = "100";
+    }
+    if (id == 3) {
+      this.race = "Human";
+      this.profession = "Cleric";
+      this.equipment = "Cursed Jewel";
+      this.strength = "5";
+      this.intelligence = "5";
+      this.dexterty = "4";
+      this.charisma = "2";
+      this.wisdom = "5";
+      this.constitution = "5";
+      this.hp = "150";
+    }
+    if (id == 4) {
+      this.race = "Elf";
+      this.profession = "Fighter";
+      this.equipment = "Bow";
+      this.strength = "5";
+      this.intelligence = "5";
+      this.dexterty = "4";
+      this.charisma = "2";
+      this.wisdom = "5";
+      this.constitution = "5";
+      this.hp = "130";
+    }
+    if (id == 5) {
+      this.race = "Gnome";
+      this.profession = "Fighter";
+      this.equipment = "Dual-Wield";
+      this.strength = "5";
+      this.intelligence = "5";
+      this.dexterty = "4";
+      this.charisma = "2";
+      this.wisdom = "5";
+      this.constitution = "5";
+      this.hp = "130";
+    }
+
   }
 
 }
